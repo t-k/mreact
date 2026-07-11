818540fa1c0109be539e0a882675ac875d371e96
Tatsuo Kaniwa <tatsuo@kaniwa.biz>
2026-07-11 22:11:19 +0900

    Prepare 0.0.191 release

--- packages/create-mreact-app/src/index.ts
+++ packages/create-mreact-app/src/index.ts
@@ -70,15 +70,15 @@ interface TemplateDefinition {
 }
 
 const internalPackageVersions = {
-  "@reckona/mreact-auth": "^0.0.190",
-  "@reckona/mreact-devtools": "^0.0.190",
-  "@reckona/mreact-forms": "^0.0.190",
-  "@reckona/mreact": "^0.0.190",
-  "@reckona/mreact-query": "^0.0.190",
-  "@reckona/mreact-reactive-core": "^0.0.190",
-  "@reckona/mreact-reactive-dom": "^0.0.190",
-  "@reckona/mreact-router": "^0.0.190",
-  "@reckona/mreact-test-utils": "^0.0.190",
+  "@reckona/mreact-auth": "^0.0.191",
+  "@reckona/mreact-devtools": "^0.0.191",
+  "@reckona/mreact-forms": "^0.0.191",
+  "@reckona/mreact": "^0.0.191",
+  "@reckona/mreact-query": "^0.0.191",
+  "@reckona/mreact-reactive-core": "^0.0.191",
+  "@reckona/mreact-reactive-dom": "^0.0.191",
+  "@reckona/mreact-router": "^0.0.191",
+  "@reckona/mreact-test-utils": "^0.0.191",
 } as const satisfies Record<string, string>;
 const currentMreactVersion = internalPackageVersions["@reckona/mreact"].replace(/^\^/, "");
 const typescriptVersion = "^7.0.2";
